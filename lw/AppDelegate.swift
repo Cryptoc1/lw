2c6d12c5a60e5b24e3dad3db8698b3da47b070da
Samuel Steele <steelesam72@gmail.com>
2016-01-05 20:25:14 -0500

    Initial Commit

---
+++ lw/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  lw
+//
+//  Created by Cryptoc1 on 1/5/16.
+//  Copyright © 2016 Cryptoc1. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ lw/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  lw
+//
+//  Created by Cryptoc1 on 1/5/16.
+//  Copyright © 2016 Cryptoc1. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
